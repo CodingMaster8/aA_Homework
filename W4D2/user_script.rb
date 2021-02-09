e27e28624c7a356f7e1e455b807075d93b0fca90
jason <jmasters1000@yahoo.com>
2021-02-08 16:51:12 -0800

    Complete Error Handling Funtime homework

---
+++ W4D2/user_script.rb
@@ -0,0 +1,10 @@
+require_relative 'super_useful'
+
+
+feed_me_a_fruit
+
+jason = BestFriend.new('jason', 8, 'poopoo')
+
+jason.talk_about_friendship
+jason.do_friendstuff
+jason.give_friendship_bracelet
