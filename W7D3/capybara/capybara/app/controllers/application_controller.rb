90b6eb61a4a3fa19c7a46d19feaca0a578a71d73
jason <jmasters1000@yahoo.com>
2021-03-05 08:39:42 -0800

    Finish capybara homework

---
+++ W7D3/capybara/capybara/app/controllers/application_controller.rb
@@ -0,0 +1,6 @@
+class ApplicationController < ActionController::Base
+
+  def is_password?
+    
+  end
+end
---
+++ W7D3/capybara/capybara/spec/models/user_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 
+
+
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password_digest) }
+    it { should validate_presence_of(:password).is_at_least(6) }
+end
